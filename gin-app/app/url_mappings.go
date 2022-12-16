b6d1e1e9104d3ba2e1a8397020122519c87b4111
m-iibuchi <m-iibuchi@highreso.co.jp>
2022-12-16 17:33:55 +0900

    feat: 商品情報更新処理を実装

--- gin-app/app/url_mappings.go
+++ gin-app/app/url_mappings.go
@@ -3,6 +3,8 @@ package app
 import "gin-sandbox/controllers/products"
 
 func mapUrls() {
-    router.GET("/products/:product_id", products.GetProduct)
-    router.POST("/products", products.CreateProduct)
+	router.GET("/products/:product_id", products.GetProduct)
+	router.POST("/products", products.CreateProduct)
+	router.PUT("/products/:product_id", products.UpdateProduct)
+	router.PATCH("/products/:product_id", products.UpdateProduct)
 }
