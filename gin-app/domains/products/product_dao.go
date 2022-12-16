f2c64f969f99be5bb4f9cb5060ea43f121ec5d81
m-iibuchi <m-iibuchi@highreso.co.jp>
2022-12-16 16:50:42 +0900

    refactor: DB周りのエラー処理を共通化

--- gin-app/domains/products/product_dao.go
+++ gin-app/domains/products/product_dao.go
@@ -1,10 +1,13 @@
 package products
 
 import (
-	"fmt"
-
 	"gin-sandbox/datasources/mysql/products_db"
 	"gin-sandbox/utils/errors"
+	"gin-sandbox/utils/mysqlutils"
+)
+
+const (
+	noSearchResult = "record not found"
 )
 
 var (
@@ -13,18 +16,14 @@ var (
 
 func (p *Product) Get() *errors.ApiErr {
 	if result := products_db.Client.Where("id = ?", p.ID).Take(&p); result.Error != nil {
-		return errors.NewInternalServerError(
-			fmt.Sprintf("error when trying to get product: %s", result.Error),
-		)
+		return mysqlutils.ParseError(result.Error)
 	}
 	return nil
 }
 
 func (p *Product) Save() *errors.ApiErr {
 	if result := products_db.Client.Create(&p); result.Error != nil {
-		return errors.NewInternalServerError(
-			fmt.Sprintf("error when trying to save product: %s", result.Error),
-		)
+		return mysqlutils.ParseError(result.Error)
 	}
 	return nil
 }
---
+++ gin-app/utils/mysqlutils/mysql_utils.go
@@ -0,0 +1,29 @@
+package mysqlutils
+
+import (
+	"gin-sandbox/utils/errors"
+	"strings"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+const (
+	noSearchResult = "record not found"
+)
+
+func ParseError(err error) *errors.ApiErr {
+	sqlErr, ok := err.(*mysql.MySQLError)
+	if !ok {
+		if strings.Contains(err.Error(), noSearchResult) {
+			return errors.NewNotFoundError("no record matching given id")
+		}
+		return errors.NewInternalServerError("error parsing database response")
+	}
+
+	switch sqlErr.Number {
+	case 1062:
+		return errors.NewBadRequestError("invalid data")
+	}
+
+	return errors.NewInternalServerError("error processing request")
+}
