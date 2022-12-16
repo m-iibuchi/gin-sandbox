d24a91a0e4e68cd6a56c6f34e532a364617ffde3
m-iibuchi <m-iibuchi@highreso.co.jp>
2022-12-16 12:24:29 +0900

    feat: リクエストエラー用の構造体を実装

--- gin-app/controllers/products/products_controller.go
+++ gin-app/controllers/products/products_controller.go
@@ -1,15 +1,12 @@
 package products
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 
 	"gin-sandbox/domains/products"
+	"gin-sandbox/utils/errors"
 )
 
 func GetProduct(c *gin.Context) {
@@ -19,19 +16,10 @@ func GetProduct(c *gin.Context) {
 func CreateProduct(c *gin.Context) {
 	// リクエストデータ取得
 	var product products.Product
-	bytes, err := ioutil.ReadAll(c.Request.Body)
-	if err != nil {
-		log.Println(err.Error())
+	if err := c.ShouldBindJSON(&product); err != nil {
+		apiErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(apiErr.Status, apiErr)
 		return
 	}
-
-	// 構造体に変換
-	if err := json.Unmarshal(bytes, &product); err != nil {
-		log.Println(err.Error())
-		return
-	}
-
-	fmt.Println(string(bytes))
-	fmt.Println(err)
-	c.JSON(http.StatusOK, product)
+	c.JSON(http.StatusCreated, product)
 }
--- gin-app/controllers/products/products_controller_test.go
+++ gin-app/controllers/products/products_controller_test.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"gin-sandbox/domains/products"
+	"gin-sandbox/utils/errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -13,22 +14,28 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCreateProductNoError(t *testing.T) {
-	// テストデータ準備
-	p := products.Product{
-		ID:   123,
-		Name: "coca cola",
-	}
-	byteProduct, _ := json.Marshal(p)
-
+func requestHandler(p interface{}) (*gin.Context, *httptest.ResponseRecorder) {
 	// リクエスト準備
 	response := httptest.NewRecorder()
+	byteProduct, _ := json.Marshal(p)
 	c, _ := gin.CreateTestContext(response)
 	c.Request, _ = http.NewRequest(
 		http.MethodPost,
 		"/products",
 		bytes.NewBuffer(byteProduct),
 	)
+	return c, response
+}
+
+func TestCreateProductNoError(t *testing.T) {
+	// テストデータ準備
+	p := products.Product{
+		ID:   123,
+		Name: "coca cola",
+	}
+
+	// リクエスト準備
+	c, response := requestHandler(p)
 
 	// リクエスト処理
 	CreateProduct(c)
@@ -38,8 +45,34 @@ func TestCreateProductNoError(t *testing.T) {
 	err := json.Unmarshal(response.Body.Bytes(), &product)
 
 	// 検証
-	assert.EqualValues(t, http.StatusOK, response.Code)
+	assert.EqualValues(t, http.StatusCreated, response.Code)
 	assert.Nil(t, err)
 	fmt.Println(product)
 	assert.EqualValues(t, uint64(123), product.ID)
+}
+
+func TestCreateProductWith404Error(t *testing.T) {
+	// テストデータ準備
+	type demiProduct struct {
+		ID   string `json: "id"`
+		Name string `json: "name"`
+	}
+	p := demiProduct{
+		ID:   "123",
+		Name: "coca cola",
+	}
+
+	// リクエスト準備
+	c, response := requestHandler(p)
+
+	// リクエスト処理
+	CreateProduct(c)
+
+	var apiErr errors.ApiErr
+	err := json.Unmarshal(response.Body.Bytes(), &apiErr)
+	assert.EqualValues(t, http.StatusBadRequest, response.Code)
+	assert.Nil(t, err)
+	assert.EqualValues(t, "invalid json body", apiErr.Message)
+	assert.EqualValues(t, 400, apiErr.Status)
+	assert.EqualValues(t, "bad_request", apiErr.Error)
 }
